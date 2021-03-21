0ec87de5721fe2f46b34e87f660046f274843f74
drivendragon <drivendragon@hotmail.com>
2021-03-20 21:21:14 -0400

    validators

--- tv_show_app/models.py
+++ tv_show_app/models.py
@@ -1,5 +1,17 @@
 from django.db import models
-    
+
+class ShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        # add keys and values to errors dictionary for each invalid field
+        if len(postData['title']) < 6:
+            errors["name"] = "Title should be at least 5 characters"
+        if len(postData['network']) < 2:
+            errors["desc"] = "Network should be at least 3 characters"
+        if len(postData['description']) < 11:
+            errors["desc"] = "Description should be at least 10 characters"
+        return errors
+
 class Shows(models.Model):
     title = models.CharField(max_length=255)
     network = models.CharField(max_length=255)
@@ -7,3 +19,4 @@ class Shows(models.Model):
     description = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
--- tv_show_app/views.py
+++ tv_show_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .models import Shows
+from django.contrib import messages
 
 
 def shows(request):
@@ -18,8 +19,21 @@ def get_show(request, id):
 def new_show(request):
   return render(request, "new_show.html")
 
+#def create(request):
+#  if request.method == "POST":
+##    newly_created_show = Shows(title=request.POST['title'], network=request.POST['network'], description=request.POST['description'], releaseDate=request.POST['releaseDate'])
+#    newly_created_show.save()
+#    return redirect(f"/shows/{newly_created_show.id}")
+#  return redirect("/shows")
+
 def create(request):
   if request.method == "POST":
+    errors = Shows.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+      # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+      for key, value in errors.items():
+        messages.error(request, value) 
+        return redirect("/shows/new")      
     newly_created_show = Shows(title=request.POST['title'], network=request.POST['network'], description=request.POST['description'], releaseDate=request.POST['releaseDate'])
     newly_created_show.save()
     return redirect(f"/shows/{newly_created_show.id}")
@@ -39,12 +53,22 @@ def show_edit(request,id):
 
 def update(request,id):
   if request.method=="POST":
-    shows = Shows.objects.get(id=id)
-    shows.title = request.POST['title']
-    shows.network = request.POST['network']
-    shows.releaseDate = request.POST['releaseDate']
-    shows.description = request.POST['description']
-    shows.save()
+    errors = Shows.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+      # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+      for key, value in errors.items():
+        messages.error(request, value) 
+        return redirect(f"/shows/{id}/edit")      
+    else:
+      # if the errors object is empty, that means there were no errors!
+      # retrieve the blog to be updated, make the changes, and save
+      shows = Shows.objects.get(id=id)
+      shows.title = request.POST['title']
+      shows.network = request.POST['network']
+      shows.releaseDate = request.POST['releaseDate']
+      shows.description = request.POST['description']
+      shows.save()
+      messages.success(request, "Show successfully updated")
   return redirect(f"/shows/{id}")
       
   
